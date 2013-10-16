a92834103733a2095f20dd392f91d557fde1fed8
Steven <fenli@fenlisproject.com>
2013-10-16 16:32:21 +0700

    test...

--- src/main/java/com/summarecon/qcapp/MainActivity.java
+++ src/main/java/com/summarecon/qcapp/MainActivity.java
@@ -9,6 +9,5 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
