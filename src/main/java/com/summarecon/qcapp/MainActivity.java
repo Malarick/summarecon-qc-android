bc5d16292020f4f9aa4b60cde5bb717ad176d3a8
Administrator <agso.87@gmail.com>
2013-10-18 20:05:31 +0700

    tes agung

--- src/main/java/com/summarecon/qcapp/MainActivity.java
+++ src/main/java/com/summarecon/qcapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.summarecon.qcapp;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(),"Tes Agung",Toast.LENGTH_SHORT);
     }
 }
